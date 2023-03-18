3b0839642b7c9a2d5da6a832de258579cfacccea
halosatrio <63773815+halosatrio@users.noreply.github.com>
2023-03-19 05:07:53 +0700

    update homepage info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import MainLayout from "@/components/MainLayout";
 
 export default function Home() {
-  return <MainLayout page="home">hehe</MainLayout>;
+  return (
+    <MainLayout page="home">
+      <h1>hehe</h1>
+      <h2>current status: under development...</h2>
+      <p>super duper minimum viable product</p>
+    </MainLayout>
+  );
 }
